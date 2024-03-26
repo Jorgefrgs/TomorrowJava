28918fc9ee251c2217bb2bb4e8ab5461dfeaf4a9
Jorgefrgs <138547654+Jorgefrgs@users.noreply.github.com>
2024-03-25 21:40:46 -0300

    Delete ExercicioJavaScrum directory

--- ExercicioJavaScrum/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.exerciciojavascrum {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.exerciciojavascrum to javafx.fxml;
-    exports com.example.exerciciojavascrum;
-}
